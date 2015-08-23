731e486c26dfb9feafb2431bc5a3dabf96cc990d
fishead <zhchuan7@gmail.com>
2015-08-23 10:51:50 +0800

    update organizer setting

--- src/organizer/setting/organizer_setting.js
+++ src/organizer/setting/organizer_setting.js
@@ -16,18 +16,57 @@ function moduleConfig($stateProvider) {
 }
 
 /* @ngInject*/
-function OrganizerSettingController(Restangular, UploadService, AlertService) {
+function OrganizerSettingController(Restangular, UploadToTempService, AlertService) {
     const vm = this;
-    vm.saveOrganizerSetting = saveOrganizerSetting;
+    const Organizer = Restangular.one('organizer');
+    vm.organizer = {
+        pictures: [],
+    };
+    vm.uploadFile = uploadFile;
+    vm.deleteNewFile = deleteNewFile;
+    vm.deleteOldFile = deleteOldFile;
+    vm.setOrganizer = setOrganizer;
     fetchSponsorConfig();
 
     function fetchSponsorConfig() {
-        vm.organizer = Restangular.one('organizer').get().$object;
+        vm.organizer = Organizer.get().$object;
     }
 
-    function saveOrganizerSetting(picture, setting) {
-        const url = '/apis/organizer';
-        UploadService(url, picture, 'pictures', setting).then(() => { // eslint-disable-line new-cap
+    function uploadFile(files) {
+        if (!files || files.length === 0) { return false; }
+        UploadToTempService.upload(files).then((fileUrls) => { // eslint-disable-line new-cap
+            const pictures = fileUrls.map(function (fileUrl) {
+                return {
+                    fileUrl: fileUrl,
+                    description: '',
+                    isNew: true,
+                };
+            });
+            vm.organizer.pictures = vm.organizer.pictures.concat(pictures);
+        }).catch((err) => {
+            AlertService.warning(err.data);
+        });
+    }
+
+    function deleteNewFile(picture, index) {
+        const filename = picture.fileUrl.split('/').pop();
+        UploadToTempService.remove(filename).then(() => {
+            vm.organizer.pictures.splice(index, 1);
+        }).catch((err) => {
+            AlertService.warning(err.data);
+        });
+    }
+
+    function deleteOldFile(picture, index) {
+        vm.organizer.one('pictures', index).remove().then(() => {
+            vm.organizer.pictures.splice(index, 1);
+        }).catch((err) => {
+            AlertService.warning(err.data);
+        });
+    }
+
+    function setOrganizer(organizer) {
+        Organizer.doPOST(organizer, '').then(() => {
             AlertService.success('设置成功');
         }).catch((err) => {
             AlertService.warning(err.data);
