c4b0f7fb8020685242a589845e51124d0b9bac3d
fishead <zhchuan7@gmail.com>
2015-08-26 14:45:25 +0800

    complete attendant

--- src/attendant/create/attendant_create.js
+++ src/attendant/create/attendant_create.js
@@ -5,30 +5,36 @@ module.exports = angular.module('ememtn.attendant.create', [
     'ui.router',
     'sanya.common.services',
 ]).config(moduleConfig)
-    .controller('TouristCreateController', TouristCreateController);
+    .controller('AttendantCreateController', AttendantCreateController);
 
 /* @ngInject */
 function moduleConfig($stateProvider) {
     $stateProvider.state('attendant-list.attendant-create', {
         url: '/_create',
         template: require('./attendant_create.html'),
-        controller: 'TouristCreateController as vm',
+        controller: 'AttendantCreateController as vm',
     });
 }
 
 /* @ngInject */
-function TouristCreateController($timeout, $scope, Restangular, AlertService) {
+function AttendantCreateController($timeout, $scope, Restangular, AlertService) {
     const vm = this;
     const Attendant = Restangular.all('attendants');
     vm.createAttendant = createAttendant;
+    vm.setPassword = setPassword;
 
     function createAttendant(attendant) {
         Attendant.post(attendant).then(function () {
-            vm.attendant = {};
+            // vm.attendant = {};
             $scope.$emit('custom.attendant.created');
             AlertService.success('创建成功');
         }).catch(function (err) {
             AlertService.warning(err.data);
         });
+    }
+
+    function setPassword(phone) {
+        if (!phone) { return; }
+        vm.attendant.password = phone.slice(-6);
     }
 }
