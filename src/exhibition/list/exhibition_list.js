29b28c23b4b778f39b94bc6ddf664ec021222413
fishead <zhchuan7@gmail.com>
2015-08-26 17:38:05 +0800

    complete exhibition list

--- src/exhibition/list/exhibition_list.js
+++ src/exhibition/list/exhibition_list.js
@@ -18,11 +18,12 @@ function moduleConfig($stateProvider) {
 }
 
 /* @ngInject*/
-function ExhibitionListController(Restangular, AlertService) {
+function ExhibitionListController($q, Restangular, AlertService) {
     const vm = this;
     const Exhibition = Restangular.all('exhibitions');
     vm.query = {};
     vm.toggleCheckAll = toggleCheckAll;
+    vm.removeCheckedExhibitions = removeCheckedExhibitions;
 
     searchExhibitions(vm.query);
 
@@ -31,8 +32,30 @@ function ExhibitionListController(Restangular, AlertService) {
     }
 
     function toggleCheckAll(checked) {
-        vm.exhibitions.forEach((exhibitions) => {
-            exhibitions.checked = checked;
+        vm.exhibitions.forEach((exhibition) => {
+            exhibition._checked = checked;
+        });
+    }
+
+    function getCheckedExhibitions() {
+        return vm.exhibitions.filter(exh => exh._checked);
+    }
+
+    function removeExhibition(exhibition) {
+        return exhibition.remove().then(() => {
+            const index = vm.exhibitions.indexOf(exhibition);
+            vm.exhibitions.splice(index, 1);
+            return $q.resolve(true);
+        });
+    }
+
+    function removeCheckedExhibitions() {
+        const exhibitions = getCheckedExhibitions();
+        const proms = exhibitions.map(removeExhibition);
+        $q.all(proms).then(() => {
+            AlertService.success('删除成功');
+        }).catch(err => {
+            AlertService.warning(err.data);
         });
     }
 }
