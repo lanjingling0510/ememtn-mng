473dd729ae732cf24795a494df68eff1264bc722
fishead <zhchuan7@gmail.com>
2015-08-27 16:22:26 +0800

    update pavilion setting

--- src/exhibition_hall/setting/exhibition_hall_setting.js
+++ src/exhibition_hall/setting/exhibition_hall_setting.js
@@ -18,54 +18,70 @@ function moduleConfig($stateProvider) {
 }
 
 /* @ngInject */
-function ExhibitionHallSettingController($scope, floors, $stateParams, Restangular, UploadService, AlertService) {
+function ExhibitionHallSettingController($scope, floors, $stateParams, Restangular, UploadToTempService, AlertService) {
     const vm = this;
     const Pavilion = Restangular.all('pavilions');
     vm.fetchPavilionByFloor = fetchPavilionByFloor;
     vm.uploadPicture = uploadPicture;
-    vm.removePicture = removePicture;
+    vm.removeNewPicture = removeNewPicture;
+    vm.removeOldPicture = removeOldPicture;
     vm.savePavilion = savePavilion;
+    $scope.$on('floor-change', onFloorChange);
+    vm.floor = floors[0];
+    fetchPavilionByFloor(vm.floor);
 
     function fetchPavilionByFloor(floor) {
-        vm.pavilion = Pavilion.one(floor).get().$object;
+        vm.pavilion = Pavilion.doGET('', {
+            JCObjId: floor.JCObjId,
+            JCObjMask: floor.JCObjMask,
+        }).$object;
     }
-
-    $scope.$on('floor-change', onFloorChange);
 
     function onFloorChange(event, data) {
         vm.floor = data.floor;
         fetchPavilionByFloor(vm.floor);
     }
 
-    function uploadPicture(picture) {
-        if (!picture) {
+    function uploadPicture(files) {
+        if (!files || files.length === 0) {
             return false;
         }
-        const url = `/apis/pavilions/${$stateParams.floor}/pictures`;
-        UploadService(url, picture, 'pictures', {}).then((result) => { // eslint-disable-line new-cap
-            const pictures = result.data.map(function (picUrl) {
+        UploadToTempService.upload(files).then((fileUrls) => { // eslint-disable-line new-cap
+            const pictures = fileUrls.map(function (fileUrl) {
                 return {
-                    fileUrl: picUrl,
+                    fileUrl: fileUrl,
                     description: '',
+                    isNew: true,
                 };
             });
-            Array.prototype.push.apply(vm.pavilion.pictures, pictures);
+            vm.pavilion.pictures = vm.pavilion.pictures.concat(pictures);
         }).catch((err) => {
             AlertService.warning(err.data);
         });
     }
 
-    function removePicture(pictureIndex) {
-        Pavilion.one(vm.pavilion.floor).one('pictures', pictureIndex).remove().then(() => {
-            vm.pavilion.pictures.splice(pictureIndex, 1);
+    function removeNewPicture(picture, index) {
+        const filename = picture.fileUrl.split('/').pop();
+        UploadToTempService.remove(filename).then(() => {
+            vm.pavilion.pictures.splice(index, 1);
+        }).catch((err) => {
+            AlertService.warning(err.data);
+        });
+    }
+
+    function removeOldPicture(picture, index) {
+        vm.pavilion.one('pictures', index).remove().then(() => {
+            vm.pavilion.pictures.splice(index, 1);
         }).catch((err) => {
             AlertService.warning(err.data);
         });
     }
 
     function savePavilion(pavilion) {
-        // PUT  /apis/pavilions/F1
-        Pavilion.one(pavilion.floor).doPUT(pavilion).then(() => {
+        pavilion.doPUT(pavilion).then(() => {
+            pavilion.pictures.forEach((pic) => {
+                pic.isNew = false;
+            });
             AlertService.success('设置成功');
         }).catch((err) => {
             AlertService.warning(err.data);
