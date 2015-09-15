d9800cfbc3a4c8015d7f2946157ea2ffbfb72cfb
fishead <zhchuan7@gmail.com>
2015-09-15 09:59:01 +0800

    render heatmap with heatmap.js instead of d3

--- src/home/home.js
+++ src/home/home.js
@@ -1,4 +1,5 @@
 const angular = require('angular');
+const h337 = require('../../node_modules/heatmap.js/heatmap.js');
 require('./home.directive.js');
 
 module.exports = angular.module('ememtn.home', [
@@ -19,5 +20,37 @@ function moduleConfig($stateProvider) {
 
 /* @ngInject */
 function HomeController() {
+    const data = [];
+    for (let i = 0, max = 18 * 14; i < max; i += 1) {
+        data.push(Math.random());
+    }
+    const columnWidth = 50;
+    const columnCount = 14;
+    const radius = Math.sqrt(Math.pow(columnWidth, 2) * 2);
+    const dataPoints = data.map((d, i) => {
+        return {
+            x: i % columnCount * columnWidth,
+            y: Math.floor(i / columnCount) * columnWidth,
+            value: d,
+        };
+    });
 
+    const hm = h337.create({
+        container: document.getElementById('heatmapContainer'),
+        radius: radius,
+        // maxOpacity: 0.5,
+        // minOpacity: 0,
+        // blur: 0.75,
+        // gradient: {
+        //     '.5': 'blue',
+        //     '.8': 'red',
+        //     '.95': 'white',
+        // },
+    });
+    hm.setData({
+        min: Math.min(...data),
+        max: Math.max(...data),
+        data: dataPoints,
+    });
+    hm.repaint();
 }
