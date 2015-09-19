a42c3032bae6c4c5709a0d23abb767457018c70f
fishead <zhchuan7@gmail.com>
2015-09-20 01:11:36 +0800

    update

---
+++ src/exhibitor/create/exhibitor_create.js
@@ -0,0 +1,95 @@
+const angular = require('angular');
+const config = require('../../config.json');
+
+module.exports = angular.module('ememtn.exhibitor.create', [
+    'ui.router',
+    'restangular',
+]).config(moduleConfig)
+    .controller('ExhibitorCreateController', ExhibitorCreateController);
+
+/* @ngInject */
+function moduleConfig($stateProvider) {
+    $stateProvider.state('exhibitor-create', {
+        url: '/exhibitors/_create',
+        template: require('./exhibitor_create.html'),
+        controller: 'ExhibitorCreateController as vm',
+    });
+}
+
+/* @ngInject*/
+function ExhibitorCreateController(Restangular, UploadToTempService, $stateParams, AlertService) {
+    const vm = this;
+    const Exhibitor = Restangular.all('exhibitors');
+    const Feature = Restangular.all('map-features');
+    vm.floors = config.floors.slice(1);
+
+    vm.uploadFile = uploadFile;
+    vm.deleteNewFile = deleteNewFile;
+    vm.submitExhibitor = submitExhibitor;
+    vm.onThunbnailUploaded = onThunbnailUploaded;
+    vm.onPictureUploaded = onPictureUploaded;
+    vm.searchFeatures = searchFeatures;
+    vm.exhibitor = {
+        floor: vm.floors[0],
+        pictures: [],
+    };
+
+    searchFeatures(vm.exhibitor.floor);
+
+    function searchFeatures(floor) {
+        vm.features = Feature.getList({
+            JCObjId: floor.JCObjId,
+            JCObjMask: floor.JCObjMask,
+            JCLayerName: 'stall',
+        }).$object;
+    }
+
+    function onThunbnailUploaded(fileUrls) {
+        // const thunbnails = fileUrls.map(function (fileUrl) {
+        //     return {
+        //         fileUrl: flileUrl,
+        //         isNew: true,
+        //     };
+        // });
+        // vm.exhibitor.thunbnails = vm.exhibitor.thunbnails.concat(thunbnails);
+        vm.exhibitor.thunbnail = {
+            fileUrl: fileUrls[0],
+            isNew: true,
+        };
+    }
+
+    function onPictureUploaded(fileUrls) {
+        const pictures = fileUrls.map(function (fileUrl) {
+            return {
+                fileUrl: fileUrl,
+                description: '',
+                isNew: true,
+            };
+        });
+        vm.exhibitor.pictures.push(...pictures);
+    }
+
+    function uploadFile(files, cb) {
+        if (!files) { return false; }
+        UploadToTempService.upload(files).then(cb).catch((err) => {
+            AlertService.warning(err.data);
+        });
+    }
+
+    function deleteNewFile(picture, index) {
+        const filename = picture.fileUrl.split('/').pop();
+        UploadToTempService.remove(filename).then(() => {
+            vm.exhibitor.pictures.splice(index, 1);
+        }).catch((err) => {
+            AlertService.warning(err.data);
+        });
+    }
+
+    function submitExhibitor(exhibitor) {
+        Exhibitor.post(exhibitor).then(() => {
+            AlertService.success('创建成功');
+        }).catch((err) => {
+            AlertService.warning(err.data);
+        });
+    }
+}
