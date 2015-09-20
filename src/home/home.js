f2ef2f9c30be92b081638f7013562b92059feeb9
fishead <zhchuan7@gmail.com>
2015-09-20 11:17:54 +0800

    show scroll bar

--- src/home/home.js
+++ src/home/home.js
@@ -19,38 +19,56 @@ function moduleConfig($stateProvider) {
 }
 
 /* @ngInject */
-function HomeController() {
-    const data = [];
-    for (let i = 0, max = 18 * 14; i < max; i += 1) {
-        data.push(Math.random());
-    }
-    const columnWidth = 50;
-    const columnCount = 14;
-    const radius = Math.sqrt(Math.pow(columnWidth, 2) * 2);
-    const dataPoints = data.map((d, i) => {
-        return {
-            x: i % columnCount * columnWidth,
-            y: Math.floor(i / columnCount) * columnWidth,
-            value: d,
-        };
-    });
+function HomeController(Restangular) {
+    const vm = this;
+    const HeatMap = Restangular.all('heat-maps');
+    vm.floors = config.floors.slice(1);
+    vm.floor = vm.floors[0];
 
-    const hm = h337.create({
-        container: document.getElementById('heatmapContainer'),
-        radius: radius,
-        // maxOpacity: 0.5,
-        // minOpacity: 0,
-        // blur: 0.75,
-        // gradient: {
-        //     '.5': 'blue',
-        //     '.8': 'red',
-        //     '.95': 'white',
-        // },
-    });
-    hm.setData({
-        min: Math.min(...data),
-        max: Math.max(...data),
-        data: dataPoints,
-    });
-    hm.repaint();
+    fetchData(vm.floor);
+
+    function paintHeat(data) {
+        // for (let i = 0, max = 18 * 14; i < max; i += 1) {
+        //     data.push(Math.random());
+        // }
+        const columnWidth = 50;
+        const columnCount = 14;
+        const radius = Math.sqrt(Math.pow(columnWidth, 2) * 2);
+        const dataPoints = data.map((d, i) => {
+            return {
+                x: i % columnCount * columnWidth,
+                y: Math.floor(i / columnCount) * columnWidth,
+                value: d,
+            };
+        });
+
+        const hm = h337.create({
+            container: document.getElementById('heatmapContainer'),
+            radius: radius,
+            // maxOpacity: 0.5,
+            // minOpacity: 0,
+            // blur: 0.75,
+            // gradient: {
+            //     '.5': 'blue',
+            //     '.8': 'red',
+            //     '.95': 'white',
+            // },
+        });
+        hm.setData({
+            min: Math.min(...data),
+            max: Math.max(...data),
+            data: dataPoints,
+        });
+        hm.repaint();
+    }
+
+    function fetchData(floor) {
+        HeatMap.getList({
+            JCObjId: floor.JCObjId,
+            JCObjMask: floor.JCObjMask,
+        }).then((heats) => {
+            vm.heats = heats;
+            paintHeat(heats);
+        });
+    }
 }
